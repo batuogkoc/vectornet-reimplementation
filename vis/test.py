f3a631f39e5aa686d2dcb5f5d7a0595382bd51e6
batuog <batu.orhun.gunduz@reateknoloji.com>
2024-06-15 13:48:08 +0300

    created argoverse visualization code, started work on dataset preprocesing

--- vis/test.py
+++
@@ -1,9 +0,0 @@
-import h5py
-import matplotlib.pyplot as plt
-
-FILE_PATH = "../AutoBots/h5_files/test_dataset.hdf5"
-
-with h5py.File(FILE_PATH, "r") as hd5_file:
-    print(list(hd5_file.keys()))
-    print(hd5_file["agents_trajectories"].shape)
-    print(hd5_file["agents_trajectories"].dtype)
