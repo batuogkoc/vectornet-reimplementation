fe3b74409a863e4e10f93b553f6f61fba0bc3c21
batuog <batu.orhun.gunduz@reateknoloji.com>
2024-06-20 09:56:31 +0300

    added requirements.txt

---
+++ requirements.py
@@ -0,0 +1,105 @@
+absl-py==2.1.0
+antlr4-python3-runtime==4.8
+-e git+https://github.com/argoai/argoverse-api.git@f886ac54fba9f06f8a7d109eb663c7f501b3aa8e#egg=argoverse
+astunparse==1.6.3
+backcall @ file:///home/conda/feedstock_root/build_artifacts/backcall_1592338393461/work
+backports.functools-lru-cache @ file:///home/conda/feedstock_root/build_artifacts/backports.functools_lru_cache_1702571698061/work
+cached-property==1.5.2
+cachetools==5.3.3
+certifi @ file:///home/conda/feedstock_root/build_artifacts/certifi_1707022139797/work/certifi
+charset-normalizer==3.3.2
+colour==0.1.5
+cycler==0.11.0
+debugpy @ file:///home/conda/feedstock_root/build_artifacts/debugpy_1649586340600/work
+decorator @ file:///home/conda/feedstock_root/build_artifacts/decorator_1641555617451/work
+descartes==1.1.0
+entrypoints @ file:///home/conda/feedstock_root/build_artifacts/entrypoints_1643888246732/work
+flatbuffers==24.3.25
+gast==0.4.0
+google-auth==2.30.0
+google-auth-oauthlib==0.4.6
+google-pasta==0.2.0
+grpcio==1.62.2
+h5py==3.3.0
+hydra-core==1.1.0
+idna==3.7
+imageio==2.31.2
+importlib-metadata==6.7.0
+importlib-resources==5.12.0
+ipykernel @ file:///home/conda/feedstock_root/build_artifacts/ipykernel_1666723258080/work
+ipython @ file:///home/conda/feedstock_root/build_artifacts/ipython_1651240553635/work
+jedi @ file:///home/conda/feedstock_root/build_artifacts/jedi_1696326070614/work
+joblib==1.3.2
+jupyter_client @ file:///home/conda/feedstock_root/build_artifacts/jupyter_client_1673615989977/work
+jupyter_core @ file:///home/conda/feedstock_root/build_artifacts/jupyter_core_1658332345782/work
+keras==2.11.0
+kiwisolver==1.4.5
+lapsolver==1.1.0
+libclang==18.1.1
+llvmlite==0.39.1
+Markdown==3.4.4
+MarkupSafe==2.1.5
+matplotlib==3.4.2
+matplotlib-inline @ file:///home/conda/feedstock_root/build_artifacts/matplotlib-inline_1713250518406/work
+motmetrics==1.1.3
+nest_asyncio @ file:///home/conda/feedstock_root/build_artifacts/nest-asyncio_1705850609492/work
+numba==0.56.4
+numpy==1.21.6
+nvidia-cublas-cu11==11.10.3.66
+nvidia-cuda-nvrtc-cu11==11.7.99
+nvidia-cuda-runtime-cu11==11.7.99
+nvidia-cudnn-cu11==8.5.0.96
+oauthlib==3.2.2
+omegaconf==2.1.0
+opencv-python==4.5.3.56
+opt-einsum==3.3.0
+packaging @ file:///home/conda/feedstock_root/build_artifacts/packaging_1696202382185/work
+pandas==1.3.1
+parso @ file:///home/conda/feedstock_root/build_artifacts/parso_1712320355065/work
+pexpect @ file:///home/conda/feedstock_root/build_artifacts/pexpect_1706113125309/work
+pickleshare @ file:///home/conda/feedstock_root/build_artifacts/pickleshare_1602536217715/work
+Pillow==9.5.0
+polars==0.18.4
+prompt-toolkit @ file:///home/conda/feedstock_root/build_artifacts/prompt-toolkit_1702399386289/work
+protobuf==3.19.6
+psutil @ file:///home/conda/feedstock_root/build_artifacts/psutil_1666155398032/work
+ptyprocess @ file:///home/conda/feedstock_root/build_artifacts/ptyprocess_1609419310487/work/dist/ptyprocess-0.7.0-py2.py3-none-any.whl
+pyasn1==0.5.1
+pyasn1-modules==0.3.0
+Pygments @ file:///home/conda/feedstock_root/build_artifacts/pygments_1700607939962/work
+pykalman==0.9.7
+pyntcloud==0.3.1
+pyparsing==3.1.2
+pyproj==3.2.1
+pyquaternion==0.9.9
+python-dateutil==2.9.0.post0
+pytz==2024.1
+PyYAML==6.0.1
+pyzmq @ file:///home/conda/feedstock_root/build_artifacts/pyzmq_1652965483789/work
+requests==2.31.0
+requests-oauthlib==2.0.0
+rsa==4.9
+scikit-learn==1.0.2
+scipy==1.7.1
+shapely==2.0.4
+six @ file:///home/conda/feedstock_root/build_artifacts/six_1620240208055/work
+tensorboard==2.11.2
+tensorboard-data-server==0.6.1
+tensorboard-plugin-wit==1.8.1
+tensorflow==2.11.0
+tensorflow-estimator==2.11.0
+tensorflow-io-gcs-filesystem==0.34.0
+termcolor==2.3.0
+threadpoolctl==3.1.0
+torch==1.13.1
+torchvision==0.14.1
+tornado @ file:///home/conda/feedstock_root/build_artifacts/tornado_1656937818679/work
+tqdm==4.66.4
+traitlets @ file:///home/conda/feedstock_root/build_artifacts/traitlets_1675110562325/work
+trajnetplusplustools==0.3.0
+typing_extensions==4.7.1
+urllib3==2.0.7
+wcwidth @ file:///home/conda/feedstock_root/build_artifacts/wcwidth_1699959196938/work
+Werkzeug==2.2.3
+wrapt==1.16.0
+zipp==3.15.0
